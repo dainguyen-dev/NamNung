78a056072ecd6eec496c78ef02d1f410cdfc9ab2
dainguyen-dev <91304099+dainguyen-dev@users.noreply.github.com>
2021-10-28 07:58:29 +0700

    develop

---
+++ NamNung.Web/CustomComponents/DashboardCustomComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Dashboards;
+using Umbraco.Web;
+using Umbraco.Web.Dashboards;
+
+namespace NamNung.Web.CustomComponents
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class DashboardCustomComponent : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Dashboards().Remove<ContentDashboard>();
+            composition.Dashboards().Remove<RedirectUrlDashboard>();
+        }
+    }
+}
